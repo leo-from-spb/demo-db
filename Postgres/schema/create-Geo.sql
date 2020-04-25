f4e65c95e0a278071f10bc94702d17ef052822f6
Leonid Bushuev <leonid.bushuev@jetbrains.com>
2020-04-25 05:57:11 +0300

    Geo: Countries_Population

--- Postgres/schema/create-Geo.sql
+++ Postgres/schema/create-Geo.sql
@@ -1,5 +1,6 @@
 set search_path to Geo, public;
 
+
 create table Country_ISO
 (
     Iso_2 char(2) not null primary key,
@@ -27,3 +28,34 @@ select Iso_2, Iso_3, Iso_Num,
        Global_Region_Code, Global_SubRegion_Code
 from Country_ISO
 with check option;
+
+
+create table Country_Population
+(
+    Iso_Num smallint not null,
+    Year smallint not null,
+    Population numeric(8,4) not null,
+    primary key (Iso_Num, Year),
+    foreign key (Iso_Num) references Country_ISO(Iso_Num)
+);
+
+
+select Iso_Num, Year, Population
+from Country_Population;
+
+
+create materialized view Country_Population_Actual as
+select distinct Iso_Num,
+                first_value(Year) over C as Year,
+                first_value(Population) over C  as Population
+from Country_Population
+window C as (partition by Iso_Num order by Year desc);
+
+refresh materialized view Country_Population_Actual;
+
+create unique index Country_Population_Actual_Iso_ui on Country_Population_Actual (Iso_Num);
+
+
+create view Country_ISO_P as
+select C.*, P.Population, P.Year as Population_Year
+from Country_ISO C join Country_Population_Actual P using (Iso_Num);
--- Postgres/schema/drop-Geo.sql
+++ Postgres/schema/drop-Geo.sql
@@ -1,5 +1,10 @@
 -- Drop Geo schema
 
-drop view Geo.Country_ISO_fn;
+drop view Geo.Country_ISO_P;
+drop view Geo.Country_ISO_FN;
+
+drop materialized view Geo.Country_Population_Actual;
 
 drop table Geo.Country_ISO;
+drop table Geo.Country_Population;
+
